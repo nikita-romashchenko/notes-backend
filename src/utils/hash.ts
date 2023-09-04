d84296a1d17b9a87cf3d174bf2e1da1dfcacff10
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2023-09-04 16:59:22 +0300

    feat: add argon2 for password hashing and hash verifying

---
+++ src/utils/hash.ts
@@ -0,0 +1,7 @@
+import * as argon2 from "argon2";
+
+const get = async (password: string) => await argon2.hash(password);
+const verify = async (hash: string, password: string) =>
+  await argon2.verify(hash, password);
+
+export { get, verify };
